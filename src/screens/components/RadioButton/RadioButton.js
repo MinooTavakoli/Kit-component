05d498b8ed3202f0c7a4a2dc5d99e27681fc9c89
Minoo <minootavakoli28@gmail.com>
2020-09-26 12:50:49 +0330

    add sass

--- src/screens/components/RadioButton/RadioButton.js
+++ src/screens/components/RadioButton/RadioButton.js
@@ -19,27 +19,27 @@ export default function RadioButton() {
         <div dir='rtl' className='header-subject'> <h3>فعالیت‌های من</h3> </div>
 
             <div dir='rtl' className='checkbox' >
-                <div className='two'>
+                <div className='title-all'>
                     <JBCheckbox label='کل فعالیت‌ها' value={input==="ALL"} onChange={e => {func('ALL' , e.target.value);}}> </JBCheckbox>
                     <h3 className='number'>215</h3>
                 </div>
 
-                <div className='one'>
+                <div className='title-create'>
                     <JBCheckbox label='محتوای ایجاد شده' value={input==="CREATED"} onChange={e => {func('CREATED' , e.target.value);}}> </JBCheckbox>
                     <h3 className='number'>126</h3>
                 </div>
 
-                <div className='two'>
+                <div className='title-edit'>
                     <JBCheckbox label='محتوای ویرایش شده' value={input==="EDITED"} onChange={e => {func('EDITED' , e.target.value);}}> </JBCheckbox>
                     <h3 className='number'>45</h3>
                 </div>
 
-                <div className='three'>
+                <div className='title-confirm'>
                     <JBCheckbox label='محتوای تایید شده' value={input==="CONFIRMED"} onChange={e => {func('CONFIRMED' , e.target.value);}}> </JBCheckbox>
                     <h3 className='number'>32</h3>
                 </div>
 
-                <div className='four'>
+                <div className='title-delete'>
                     <JBCheckbox label='محتوای حذف شده' value={input==="DELETED"} onChange={e => {func('DELETED' , e.target.value);}}> </JBCheckbox>
                     <h3 className='number'>12</h3>
                 </div>
